b4bbc3cd95865aba625d94f3bc862a8a3756b9a2
Stefano Fiorentino <stefano.fiore84@gmail.com>
2019-01-14 23:05:46 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <type_traits>
+#include <tuple>
+#include <jsoncpp/json/json.h>
+
+// https://stackoverflow.com/a/34091881/2794395
+
+template<typename Class, typename T>
+struct Property
+{
+    constexpr Property(T Class::*aMember, const char *aName) :
+            member{aMember}, name{aName}
+    {}
+
+    using Type = T;
+
+    T Class::*member;
+    const char *name;
+};
+
+struct User
+{
+    std::string username;
+    std::string info;
+
+    constexpr static auto properties = std::make_tuple(Property<User, std::string>{&User::username, "username"},
+                                                       Property<User, std::string>{&User::info, "info"});
+};
+
+template<std::size_t iteration, typename T>
+void doSetData(T &&object, const Json::Value &data)
+{
+    constexpr auto property = std::get<iteration>(std::decay_t<T>::properties);
+    using Type = typename decltype(property)::Type;
+    object.*(property.member) = data[property.name].asString();
+}
+
+template<std::size_t iteration, typename T, typename empty = std::enable_if_t<(iteration > 0)>>
+void setData(T &&object, const Json::Value &data)
+{
+    doSetData<iteration>(object, data);
+    setData<iteration - 1>(object, data);
+}
+
+template<std::size_t iteration, typename T, typename empty = std::enable_if_t<(iteration == 0)>>
+void setData(T &&object, const Json::Value &data)
+{
+    doSetData<iteration>(object, data);
+}
+
+template<typename T>
+T fromJson(Json::Value data)
+{
+    T object;
+
+    setData<std::tuple_size<decltype(T::properties)>::value - 1>(object, data);
+
+    return object;
+}
+
+int main()
+{
+    Json::Value value;
+    value["username"] = "fiorentinoing";
+    value["info"] = "";
+    User u = fromJson<User>(value);
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
